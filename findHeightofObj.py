14c572a1bce1988947fcf3fd3f0b3e9e37ea6de5
Felix Dahle <felix.dahle@googlemail.com>
2019-06-28 13:39:15 +0200

    Create findHeightofObj.py

---
+++ findHeightofObj.py
@@ -0,0 +1,27 @@
+"""
+@author: Yames Jifang
+"""
+
+import os
+import json
+
+if __name__ == "__main__":
+
+    //path to folder with obj files
+    basepath = ""
+    
+    //path to json file
+    jsonFile = ''
+    
+    heights = {}
+    for entry in os.listdir(basepath):
+        with open(os.path.join(basepath, entry), 'r') as obj:
+            z = []
+            for line in obj.readlines():
+                if line.split(' ')[0] == 'v':
+                    z.append(float(line.split(' ')[-1]))
+            heights[entry.split('.')[0]] = max(z)
+    
+    print("Find heights for {} buildings.\nSave them in {}.".format(len(heights), jsonFile))
+    with open(jsonFile, 'w') as f:
+        json.dump(heights, f)
